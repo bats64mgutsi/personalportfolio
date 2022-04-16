5204990907523bcb68071b485e06a8a8d4ce8be6
Batandwa Mgutsi <b.mgutsi@gmail.com>
2022-04-16 21:11:40 +0200

    Updates frontend to use latest database structure

--- motionlink.config.js
+++ motionlink.config.js
@@ -1,6 +1,31 @@
 const markdownService = require("motionlink-cli/lib/services/markdown_service");
 const ObjectTransformers = markdownService.ObjectTransformers;
+const BlockTransformers = markdownService.BlockTransformers;
+
 const showdown = require("showdown");
+
+const allFilter = {
+  or: [
+    {
+      property: "Status",
+      select: {
+        equals: "Published",
+      },
+    },
+    {
+      property: "Status",
+      select: {
+        equals: "Completed",
+      },
+    },
+    {
+      property: "Status",
+      select: {
+        equals: "Public",
+      },
+    },
+  ],
+};
 
 /** @type {import("motionlink-cli/lib/models/config_models").TemplateRule[]} */
 const rules = [
@@ -11,12 +36,27 @@ const rules = [
       database: "personalInformation",
       takeOnly: 1,
       fetchBlocks: true,
+      filter: allFilter,
       map: (page, ctx) => {
-        page.otherData.resumeLink = "";
-
         const aboutMeMarkdown = ctx.genMarkdownForBlocks(page.blocks);
 
-        page.otherData.aboutMe = new showdown.Converter().makeHtml(aboutMeMarkdown);
+        page.otherData.aboutMe = new showdown.Converter().makeHtml(
+          aboutMeMarkdown
+        );
+
+        page.otherData.personName = ObjectTransformers.transform_all(
+          page.data.properties.Name.title
+        );
+        page.otherData.headline = ObjectTransformers.transform_all(
+          page.data.properties.Headline.rich_text
+        );
+
+        page.otherData.year = new Date().getFullYear();
+        page.otherData.email = page.data.properties.Email.email;
+        page.otherData.github = page.data.properties.Github.url;
+        page.otherData.linkedin = page.data.properties.Linkedin.url;
+        page.otherData.twitter = page.data.properties.Twitter.url;
+        page.otherData.medium = page.data.properties.Medium.url;
 
         page.otherData.topSkills =
           page.data.properties.TopSkills.multi_select.map(
@@ -43,18 +83,16 @@ const rules = [
       {
         database: "education",
         fetchBlocks: false,
-        map: (page, _) => {
+        map: (page, ctx) => {
           page.otherData.institution = ObjectTransformers.transform_all(
-            page.data.properties.Institution.title
+            page.data.properties.Name.title
           );
 
           page.otherData.qualification = ObjectTransformers.transform_all(
             page.data.properties.Qualification.rich_text
           );
 
-          page.otherData.description = ObjectTransformers.transform_all(
-            page.data.properties.Description.rich_text
-          );
+          page.otherData.description = ctx.genMarkdownForBlocks(page.blocks);
 
           page.otherData.period = ObjectTransformers.transform_all(
             page.data.properties.Period.rich_text
@@ -65,23 +103,21 @@ const rules = [
       },
       {
         database: "experience",
-        fetchBlocks: false,
+        fetchBlocks: true,
+        filter: allFilter,
         sort: [
           {
             timestamp: "created_time",
             direction: "descending",
           },
         ],
-        map: (page, _) => {
+        map: (page, ctx) => {
           page.otherData.companyName = ObjectTransformers.transform_all(
-            page.data.properties.CompanyName.title
+            page.data.properties.Name.title
           );
 
           page.otherData.role = page.data.properties.Role.select.name;
-
-          page.otherData.description = ObjectTransformers.transform_all(
-            page.data.properties.Description.rich_text
-          );
+          page.otherData.description = ctx.genMarkdownForBlocks(page.blocks);
 
           page.otherData.period = ObjectTransformers.transform_all(
             page.data.properties.Period.rich_text
@@ -93,27 +129,61 @@ const rules = [
       {
         database: "latestProjects",
         takeOnly: 3,
-        fetchBlocks: false,
+        fetchBlocks: true,
+        filter: allFilter,
         sort: [
           {
             timestamp: "last_edited_time",
             direction: "descending",
           },
         ],
-        map: (page, ctx) => {
+        map: (page, context) => {
           page.otherData.name = ObjectTransformers.transform_all(
             page.data.properties.Name.title
           );
 
-          page.otherData.logoUrl = ctx.fetchMedia(
-            page.data.properties.Logo.files[0]
-          ).src;
+          // The page thumbnail is the first image in the page
+          const defaultImageBlockTransformer = BlockTransformers.image;
 
-          page.otherData.description = ObjectTransformers.transform_all(
-            page.data.properties.Description.rich_text
+          /**@type {import("motionlink-cli/lib/models/config_models").NotionBlock}*/
+          let thumbnailBlock = undefined;
+          BlockTransformers.image = (block, rule) => {
+            if (block.type === "image") {
+              if (thumbnailBlock === undefined) {
+                thumbnailBlock = {
+                  data: block,
+                  children: [],
+                };
+
+                return "";
+              } else {
+                return defaultImageBlockTransformer(block, rule);
+              }
+            }
+
+            return "";
+          };
+
+          page.otherData.description = context.genMarkdownForBlocks(
+            page.blocks
           );
 
-          page.otherData.url = page.data.properties.Url.url;
+          if (thumbnailBlock === undefined) {
+            throw new Error(
+              "This page does not have a thumbnail. All pages must have a thumbnail."
+            );
+          }
+
+          if (thumbnailBlock.data.type === "image") {
+            const thumbnailMedia = context.fetchMedia(
+              // @ts-ignore
+              thumbnailBlock.data.image
+            );
+
+            page.otherData.logoUrl = thumbnailMedia.src;
+          }
+
+          page.otherData.url = page.data.properties.Link.url;
           return page;
         },
       },
